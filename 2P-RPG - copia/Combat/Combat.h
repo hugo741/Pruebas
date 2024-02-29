53e6564971dc92ec0fb8eb5a080d114b72370fc3
hugo741 <141595327+hugo741@users.noreply.github.com>
2024-02-28 20:28:52 -0600

    Add files via upload

---
+++ 2P-RPG - copia/Combat/Combat.h
@@ -0,0 +1,29 @@
+//
+// Created by Victor Navarro on 19/02/24.
+//
+
+#ifndef RPG_COMBAT_H
+#define RPG_COMBAT_H
+#include <vector>
+#include "../Character/Character.h"
+#include "../Player/Player.h"
+#include "../Enemy/Enemy.h"
+
+class Combat {
+private:
+    vector<Character*> participants;
+    vector<Player*> partyMembers;
+    vector<Enemy*> enemies;
+
+    void combatPrep();
+    Character* getTarget(Character* attacker);
+public:
+    Combat(vector<Character*> _participants);
+    Combat(vector<Player*> _partyMembers, vector<Enemy*> _enemies);
+    Combat();
+    void doCombat();
+    void addParticipant(Character *participant);
+    string toString();
+};
+
+#endif //RPG_COMBAT_H
---
+++ 2P-RPG - copia/Player/Player.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Victor Navarro on 15/02/24.
+//
+
+#include "Player.h"
+#include <iostream>
+
+using namespace std;
+
+Player::Player(string _name, int _health, int _attack, int _defense, int _speed) : Character(_name, _health, _attack, _defense, _speed, true) {
+    level = 1;
+    experience = 0;
+}
+
+void Player::doAttack(Character *target) {
+    target->takeDamage(attack);
+}
+
+void Player::takeDamage(int damage) {
+    int trueDamage = damage - defense;
+    health-= trueDamage;
+    cout << name << " took " << trueDamage << " damage!" << endl;
+
+}
+
+void Player::getDefense(double defend) {
+    defend = (0.2);
+   int cover = static_cast<int>(defend * defense);
+   defense += cover;
+}
+
+void Player::levelUp() {
+    level++;
+}
+
+void Player::gainExperience(int exp) {
+    experience += exp;
+    if (experience >= 100) {
+        levelUp();
+        experience = 100-experience;
+    }
+}
+
+Character* Player::selectTarget(vector<Enemy*> possibleTargets) {
+    int selectedTarget = 0;
+    cout << "Select a target: " << endl;
+    for (int i = 0; i < possibleTargets.size(); i++) {
+        cout << i << ". " << possibleTargets[i]->getName() << endl;
+    }
+
+    //TODO: Add input validation
+    cin >> selectedTarget;
+    return possibleTargets[selectedTarget];
+}
